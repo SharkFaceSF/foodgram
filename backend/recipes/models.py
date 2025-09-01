7e126eef72039c8ab8533935313b770df743a5c0
Максим Рождественский <sharkfacesf34@gmail.com>
2025-09-01 07:37:04 +0300

    ver.1 BaseProject

---
+++ backend/recipes/models.py
@@ -0,0 +1,63 @@
+from django.db import models
+
+from django.db import models
+from django.core.validators import MinValueValidator
+from users.models import User
+
+class Tag(models.Model):
+    name = models.CharField(max_length=200, unique=True)
+    slug = models.SlugField(max_length=200, unique=True)
+
+    class Meta:
+        ordering = ['name']
+
+    def __str__(self):
+        return self.name
+
+class Ingredient(models.Model):
+    name = models.CharField(max_length=200)
+    measurement_unit = models.CharField(max_length=200)
+
+    class Meta:
+        ordering = ['name']
+        unique_together = ['name', 'measurement_unit']
+
+    def __str__(self):
+        return f"{self.name}, {self.measurement_unit}"
+
+class Recipe(models.Model):
+    author = models.ForeignKey(User, on_delete=models.CASCADE, related_name='recipes')
+    name = models.CharField(max_length=200)
+    image = models.ImageField(upload_to='recipes/images/')
+    text = models.TextField()
+    cooking_time = models.PositiveIntegerField(validators=[MinValueValidator(1)])
+    tags = models.ManyToManyField(Tag, related_name='recipes')
+    pub_date = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        ordering = ['-pub_date']
+
+    def __str__(self):
+        return self.name
+
+class RecipeIngredient(models.Model):
+    recipe = models.ForeignKey(Recipe, on_delete=models.CASCADE, related_name='ingredients')
+    ingredient = models.ForeignKey(Ingredient, on_delete=models.PROTECT)
+    amount = models.PositiveIntegerField(validators=[MinValueValidator(1)])
+
+    class Meta:
+        unique_together = ['recipe', 'ingredient']
+
+class Favorite(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='favorites')
+    recipe = models.ForeignKey(Recipe, on_delete=models.CASCADE, related_name='favorited_by')
+
+    class Meta:
+        unique_together = ['user', 'recipe']
+
+class ShoppingCart(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='shopping_cart')
+    recipe = models.ForeignKey(Recipe, on_delete=models.CASCADE, related_name='in_cart')
+
+    class Meta:
+        unique_together = ['user', 'recipe']
---
+++ backend/users/models.py
@@ -0,0 +1,24 @@
+from django.contrib.auth.models import AbstractUser
+from django.db import models
+
+class User(AbstractUser):
+    email = models.EmailField(unique=True, max_length=254)
+    username = models.CharField(unique=True, max_length=150)
+    first_name = models.CharField(max_length=150)
+    last_name = models.CharField(max_length=150)
+    avatar = models.ImageField(upload_to='users/avatars/', null=True, blank=True)
+
+    REQUIRED_FIELDS = ['email', 'first_name', 'last_name']
+
+    class Meta:
+        ordering = ['username']
+
+class Follow(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='follower')
+    author = models.ForeignKey(User, on_delete=models.CASCADE, related_name='following')
+
+    class Meta:
+        unique_together = ['user', 'author']
+        constraints = [
+            models.CheckConstraint(check=~models.Q(user=models.F('author')), name='no_self_follow')
+        ]
