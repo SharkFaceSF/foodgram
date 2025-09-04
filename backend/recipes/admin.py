3c2f22115470849395f0b78e139b08a4b22c4223
Максим Рождественский <sharkfacesf34@gmail.com>
2025-09-04 17:36:39 +0300

    ver.3 AdminPanel

--- backend/recipes/admin.py
+++ backend/recipes/admin.py
@@ -1,9 +1,48 @@
 from django.contrib import admin
 from .models import Tag, Ingredient, Recipe, RecipeIngredient, Favorite, ShoppingCart
 
-admin.site.register(Tag)
-admin.site.register(Ingredient)
-admin.site.register(Recipe)
-admin.site.register(RecipeIngredient)
-admin.site.register(Favorite)
-admin.site.register(ShoppingCart)
+class RecipeIngredientInline(admin.TabularInline):
+    model = RecipeIngredient
+    extra = 1
+    min_num = 1
+
+@admin.register(Tag)
+class TagAdmin(admin.ModelAdmin):
+    list_display = ('name', 'slug')
+    search_fields = ('name',)
+    ordering = ('name',)
+
+@admin.register(Ingredient)
+class IngredientAdmin(admin.ModelAdmin):
+    list_display = ('name', 'measurement_unit')
+    search_fields = ('name',)
+    ordering = ('name',)
+
+@admin.register(Recipe)
+class RecipeAdmin(admin.ModelAdmin):
+    list_display = ('name', 'author', 'favorites_count')
+    search_fields = ('name', 'author__username', 'author__email')
+    list_filter = ('tags',)
+    inlines = [RecipeIngredientInline]
+    ordering = ('-pub_date',)
+
+    def favorites_count(self, obj):
+        return obj.favorited_by.count()
+    favorites_count.short_description = 'Добавлений в избранное'
+
+@admin.register(RecipeIngredient)
+class RecipeIngredientAdmin(admin.ModelAdmin):
+    list_display = ('recipe', 'ingredient', 'amount')
+    search_fields = ('recipe__name', 'ingredient__name')
+
+@admin.register(Favorite)
+class FavoriteAdmin(admin.ModelAdmin):
+    list_display = ('user', 'recipe')
+    search_fields = ('user__username', 'recipe__name')
+    list_filter = ('user',)
+
+@admin.register(ShoppingCart)
+class ShoppingCartAdmin(admin.ModelAdmin):
+    list_display = ('user', 'recipe')
+    search_fields = ('user__username', 'recipe__name')
+    list_filter = ('user',)
--- backend/users/admin.py
+++ backend/users/admin.py
@@ -1,5 +1,16 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
 from .models import User, Follow
 
-admin.site.register(User)
-admin.site.register(Follow)
+@admin.register(User)
+class CustomUserAdmin(UserAdmin):
+    list_display = ('username', 'email', 'first_name', 'last_name', 'avatar')
+    search_fields = ('email', 'username')
+    list_filter = ('email', 'username')
+    ordering = ('username',)
+
+@admin.register(Follow)
+class FollowAdmin(admin.ModelAdmin):
+    list_display = ('user', 'author')
+    search_fields = ('user__username', 'author__username')
+    list_filter = ('user', 'author')
